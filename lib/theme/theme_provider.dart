8ce526bfa60d013264f4ebd2ba890d90b41a5c00
wasib hussain <wasibhussain3@gmail.com>
2024-09-13 10:47:39 +0500

    theme issue fixed -

--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:portfolio_site/core/local_data_source.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  bool isDarkTheme = false;
-  changeTheme() {
-    isDarkTheme = !isDarkTheme;
-    LocalDataSource().setThemeData(isDarkTheme);
+  bool _isDarkTheme = false;
+
+  bool get isDarkTheme => _isDarkTheme;
+
+  set isDarkTheme(bool value) {
+    _isDarkTheme = value;
     notifyListeners();
+  }
+
+  // Change the theme and persist it using SharedPreferences
+  void changeTheme() async {
+    _isDarkTheme = !_isDarkTheme;
+    notifyListeners();
+    await LocalDataSource.setThemeData(_isDarkTheme);
   }
 }
